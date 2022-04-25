f7c64b6405051522d89ff064d6eeb3977f031247
Tuan Tai <nguyentuantai412000@gmail.com>
2022-04-25 17:08:30 +0700

    Example new UI

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import BookingPage from './Booking';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<h1>Home</h1>
+			<Routes>
+				<Route path='/rooms' element={<BookingPage />} exact />
+			</Routes>
+
+			<ToastContainer autoClose={3000} theme='colored' />
 		</div>
 	);
 };
