c49e78bdf279dfdcfeafebed750fa09785a88486
Tuan Tai <nguyentuantai412000@gmail.com>
2022-06-22 21:15:42 +0700

    loading when check available

--- src/containers/Home/Room/index.jsx
+++ src/containers/Home/Room/index.jsx
@@ -12,7 +12,8 @@ import { getDateRange } from '../../../utils/convertDateTime';
 
 import './style.scss';
 import { getAllRoom, checkAvailable } from './../../../redux/actions/room';
-import { setBooking } from '../../../redux/actions/booking';
+
+import FullLoading from './../../../components/Common/FullLoading/index';
 
 const capacityDefault = {
 	adult: 1,
@@ -26,6 +27,7 @@ const RoomPage = () => {
 		new Date(),
 		new Date(new Date().getTime() + 12 * 60 * 60 * 1000 * 2),
 	]);
+
 	const [startDate, endDate] = dateRange;
 	const [dayDiff, setDayDiff] = useState(getDateRange(startDate, endDate));
 	const [capacity, setCapacity] = useState({ ...capacityDefault });
@@ -35,6 +37,7 @@ const RoomPage = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const listRoom = useSelector((state) => state.roomReducer.rooms);
+	const isLoading = useSelector((state) => state.roomReducer.isRoomLoading);
 
 	useEffect(() => {
 		dispatch(getAllRoom());
@@ -122,96 +125,103 @@ const RoomPage = () => {
 		</button>
 	));
 	const totalChild = capacity.childOver12 + capacity.childUnder12;
+
 	return (
 		<div className='room-page'>
-			<section className='row select-booking'>
-				<div className='col-5 flex-center'>
-					<span className='web-name'>Rooms</span>
-				</div>
-				<div className='col-7'>
-					<div className='row'>
-						<div className='col-6 select-date'>
-							<label className='text-title'>Check-in/Check-out</label>
-							<DatePicker
-								selectsRange={true}
-								startDate={startDate}
-								endDate={endDate}
-								onChange={onChangeDateRange}
-								customInput={<ExampleCustomInput />}
-							/>
+			{isLoading ? (
+				<FullLoading />
+			) : (
+				<>
+					<section className='row select-booking'>
+						<div className='col-5 flex-center'>
+							<span className='web-name'>Rooms</span>
 						</div>
-						<div className='col-6 select-people'>
-							<SelectNumberPeople
-								value={capacity.adult}
-								name='adult'
-								onChangeSelect={onChangeCapacity}
-							/>
-							<SelectNumberPeople
-								value={capacity.child}
-								name='child'
-								adult={capacity.adult}
-								onChangeSelect={onChangeCapacity}
-							/>
-							{capacity && capacity.child > 0 && (
-								<div className='old-of-child format-default row'>
-									<div className='mb-3 col-6'>
-										<label htmlFor='under12'>Child under 12</label>
-										<input
-											type='number'
-											className='form-control input-text-home'
-											min={0}
-											id='under12'
-											value={capacity.childUnder12}
-											name='childUnder12'
-											onChange={onChangeOldChild}
-											disabled={totalChild === capacity.child}
-										/>
-									</div>
-									<div className='mb-3 col-6'>
-										<label htmlFor='over12'>Child over 12</label>
-										<input
-											type='number'
-											className='form-control input-text-home'
-											min={0}
-											id='over12'
-											name='childOver12'
-											value={capacity.childOver12}
-											onChange={onChangeOldChild}
-											disabled={totalChild === capacity.child}
-										/>
-									</div>
+						<div className='col-7'>
+							<div className='row'>
+								<div className='col-6 select-date'>
+									<label className='text-title'>Check-in/Check-out</label>
+									<DatePicker
+										selectsRange={true}
+										startDate={startDate}
+										endDate={endDate}
+										onChange={onChangeDateRange}
+										customInput={<ExampleCustomInput />}
+									/>
 								</div>
-							)}
+								<div className='col-6 select-people'>
+									<SelectNumberPeople
+										value={capacity.adult}
+										name='adult'
+										onChangeSelect={onChangeCapacity}
+									/>
+									<SelectNumberPeople
+										value={capacity.child}
+										name='child'
+										adult={capacity.adult}
+										onChangeSelect={onChangeCapacity}
+									/>
+									{capacity && capacity.child > 0 && (
+										<div className='old-of-child format-default row'>
+											<div className='mb-3 col-6'>
+												<label htmlFor='under12'>Child under 12</label>
+												<input
+													type='number'
+													className='form-control input-text-home'
+													min={0}
+													id='under12'
+													value={capacity.childUnder12}
+													name='childUnder12'
+													onChange={onChangeOldChild}
+													disabled={totalChild === capacity.child}
+												/>
+											</div>
+											<div className='mb-3 col-6'>
+												<label htmlFor='over12'>Child over 12</label>
+												<input
+													type='number'
+													className='form-control input-text-home'
+													min={0}
+													id='over12'
+													name='childOver12'
+													value={capacity.childOver12}
+													onChange={onChangeOldChild}
+													disabled={totalChild === capacity.child}
+												/>
+											</div>
+										</div>
+									)}
+								</div>
+							</div>
+							<div className='flex-center'>
+								<span className='btn-home' onClick={onCheckAvailability}>
+									Check availability
+								</span>
+							</div>
 						</div>
-					</div>
-					<div className='flex-center'>
-						<span className='btn-home' onClick={onCheckAvailability}>
-							Check availability
-						</span>
-					</div>
-				</div>
-			</section>
-			{Array.isArray(listRoom) && listRoom.length > 0 && (
-				<section className='available-room'>
-					<div className='list-room row'>
-						<div className='col-9'>
-							{listRoom.map((room) => (
-								<AvailableRoom room={room} key={room._id} onSelect={onSelectedRoom} />
-							))}
-						</div>
+					</section>
+					{Array.isArray(listRoom) && listRoom.length > 0 && (
+						<section className='available-room'>
+							<div className='list-room row'>
+								<div className='col-9'>
+									{listRoom.map((room) => (
+										<AvailableRoom room={room} key={room._id} onSelect={onSelectedRoom} />
+									))}
+								</div>
 
-						<div className='btn-booking col-3 pos-fixed end-0'>
-							<button onClick={onBooking} disabled={!(selectRoom.length > 0)}>
-								Book Now
-							</button>
-							<p>{selectRoom && selectRoom.length} room</p>
-							<RoomPrice
-								price={totalPrice ? totalPrice : 0}
-								message={`for ${dayDiff ? dayDiff : 0} night`}
-							/>
-						</div>
-					</div>
-				</section>
+								<div className='btn-booking col-3 pos-fixed end-0'>
+									<button onClick={onBooking} disabled={!(selectRoom.length > 0)}>
+										Book Now
+									</button>
+									<p>{selectRoom && selectRoom.length} room</p>
+									<RoomPrice
+										price={totalPrice ? totalPrice : 0}
+										message={`for ${dayDiff ? dayDiff : 0} night`}
+									/>
+								</div>
+							</div>
+						</section>
+					)}
+				</>
 			)}
 		</div>
 	);
